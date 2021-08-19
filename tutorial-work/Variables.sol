31f733c65f31baa28e3020c130a7ea4c838e9b89
Greg Zucchero <gzucchero24@gmail.com>
2021-08-19 17:18:06 -0500

    first commit

---
+++ tutorial-work/Variables.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.1;
+
+contract Variables {
+    
+    bool Telato = false;
+    
+    
+    uint8 number8 = 9;
+    uint64 number64 = 549;
+    uint128 number128 = 39485839096;
+    uint256 number256 = 7590487659043;
+    
+    uint number = 43278590845;
+    
+    int256 negnumber256 = -2904732907;
+    int negnumber = -35743908;
+    
+    string basicString = "Hello";
+    
+    string [] public stringArray = ["hi", "hey"];
+    
+}
